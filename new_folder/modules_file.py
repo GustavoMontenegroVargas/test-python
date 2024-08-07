6a4c773a5a164043b8cde0cd7f736b8678c4c149
Gustavo Montenegro <gustavomontenegro@MacBook-Air-de-Gustavo.local>
2024-08-06 20:00:37 -0600

    new changes

--- new_folder/modules_file.py
+++ new_folder/modules_file.py
@@ -1,3 +1,11 @@
 def my_test_function():
 	print("I'm working as exptected")
 
+def scaling_data(data):
+	from sklearn.preprocessing import StandardScaler
+	scaler = StandardScaler()
+	scaled_data = scaler.fit_transform(data)
+
+	return scaled_data
+
+
